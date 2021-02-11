7f10b6fa99e04d310cf8d6c6e5d532782439e853
YuyaoYolandaCheng <56518914+YuyaoYolandaCheng@users.noreply.github.com>
2021-02-11 07:08:06 -0800

    Add files via upload

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,68 @@
 // product-item.js
 
-class ProductItem extends HTMLElement {
-  // TODO
+myStorage = window.localStorage;
+var cart_count = document.getElementById('cart-count');
+var in_cart = JSON.parse(myStorage.getItem('in_cart'));
+    
+class ProductItem extends HTMLElement 
+{
+	constructor(id, image, description, title, price)
+	{
+		super();
+		
+		var shadow = this.attachShadow({mode: 'open'});
+		
+    var list = document.createElement('list');
+    var img = document.createElement('img');
+    var name = document.createElement('p');
+    var price_tag = document.createElement('p');
+    var click = document.createElement('button');
+
+
+    list.setAttribute('class', 'product');
+    
+    img.alt = description;
+		img.src = image;
+		
+    name.innerHTML = title;
+    name.setAttribute('class', 'title');
+		
+    price_tag.innerHTML = '$' + price;
+    price_tag.setAttribute('class', 'price');
+		
+    if(in_cart.includes(id)){
+      click.innerHTML = "Remove from Cart";
+    }else{
+      click.innerHTML = "Add to Cart";
+    }
+
+		click.onclick = function (){
+			if (click.innerHTML == "Add to Cart"){
+				click.innerHTML = "Remove from Cart";
+				cart_count.innerHTML = parseInt(cart_count.innerHTML) + 1;
+				in_cart.push(id);
+			}
+			else{
+				click.innerHTML = "Add to Cart";
+				cart_count.innerHTML = parseInt(cart_count.innerHTML) - 1;
+				in_cart.splice(in_cart.indexOf(id), 1);
+      }
+      myStorage.setItem("in_cart", JSON.stringify(in_cart));
+		}
+    
+    list.appendChild(img);
+		list.appendChild(name);
+		list.appendChild(price_tag);
+		list.appendChild(click);
+    
+    var link = document.createElement('link');
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = "./styles/styles.css";
+    document.querySelector("head").appendChild(link);
+		shadow.appendChild(link);
+    shadow.appendChild(list);
+	}
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,28 @@
 // Script.js
 
-window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+myStorage = window.localStorage;
+var product_list_container = document.getElementById('product-list');
+var cart_count = document.getElementById('cart-count');
+
+window.addEventListener('DOMContentLoaded', () => 
+{
+	if (!myStorage.getItem('products')){
+		fetch('https://fakestoreapi.com/products')
+			.then(response => response.json())
+			.then(data => {
+				myStorage.setItem('products', JSON.stringify(data))
+			})
+  }
+  
+  if (!myStorage.getItem('in_cart')){
+		myStorage.setItem('in_cart', JSON.stringify([]))
+	}
+	var in_cart = JSON.parse(myStorage.getItem('in_cart'));
+	cart_count.innerHTML = in_cart.length;
+  
+  var items = JSON.parse(myStorage.getItem('products'));
+	for (var i = 0; i < items.length; i++){
+		var item = new ProductItem(items[i]['id'], items[i]['image'], items[i]['description'], items[i]['title'], items[i]['price']);
+		product_list_container.appendChild(item);
+	}	
 });
